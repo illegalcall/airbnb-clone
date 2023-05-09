6c84d28eb26e81c9321d1cf482b9036178cc828c
Dhruv Sharma <dhruvsharma@cult>
2023-05-09 13:43:43 +0530

    feat: navbar ui implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
+import { Navbar } from './components/organisms';
 
 export default function Home() {
 	return (
-		<div className='text-rose-500 text-2xl'>Hello</div>
+		<div className='text-rose-500 text-2xl'>
+			<Navbar />
+		</div>
 	);
 }
